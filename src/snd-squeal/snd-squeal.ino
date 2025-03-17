96d02932351e2d84178c85f7caf7259d1d5312c3
Michael Petersen <railfan@drgw.net>
2025-03-16 20:59:00 -0600

    Added header

--- src/snd-squeal/snd-squeal.ino
+++ src/snd-squeal/snd-squeal.ino
@@ -1,3 +1,24 @@
+/*************************************************************************
+Title:    Squealer
+Authors:  Michael Petersen <railfan@drgw.net>
+File:     snd-squeal.ino
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2024 Michael Petersen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*************************************************************************/
+
 #include <SPI.h>
 #include <SD.h>
 #include "driver/i2s.h"
